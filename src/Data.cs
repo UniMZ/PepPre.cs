5b63d51c680f7945cace9e0a58b6536f5eb6e356
Tarn Yeong Ching <i@ctarn.io>
2025-08-04 17:04:07 +0800

    load ms (#5)

---
+++ src/Data.cs
@@ -0,0 +1,42 @@
+﻿namespace UniMZ.PepPre;
+
+using Microsoft.Data.Analysis;
+using UniMZ.Core;
+using MS1 = (int[] id, Memory<Core.Peak<double>>[] peak);
+using MS2 = (int[] id, Memory<Core.Peak<double>>[] peak, int[] pre, double[] mz, double[] r);
+
+public static partial class PepPre
+{
+    public static DataFrame ReadMS(string path)
+    {
+        // TODO: handle other format
+        // should be sorted by `ScanID`.
+        return path.ReadUMZ();
+    }
+
+    public static MS1 ToMS1(DataFrame df, string id = "ScanID", string peak = "Peak")
+    {
+        var id_ = df[id].ToArray<int>();
+        var peak_ = df[peak].ToMemoryArray<Peak<double>>();
+        return (id_, peak_);
+    }
+
+    public static MS2 ToMS2(DataFrame df, string id = "ScanID", string peak = "Peak", string pre = "PrecursorScan",
+        string mz = "ActivationCenter", string r = "IsolationWidth")
+    {
+        var id_ = df[id].ToArray<int>();
+        var peak_ = df[peak].ToMemoryArray<Peak<double>>();
+        var pre_ = df[pre].ToArray<int>();
+        var mz_ = df[mz].ToArray<double>();
+        var r_ = df[r].ToArray<double>();
+        return (id_, peak_, pre_, mz_, r_);
+    }
+
+    public static (MS1 ms1, MS2 ms2) ToMS(DataFrame df, string type = "ScanType", string ms1 = "MS1",
+        string ms2 = "MS2")
+    {
+        var ms1_ = df.Filter(df.Columns[type].ElementwiseEquals(ms1));
+        var ms2_ = df.Filter(df.Columns[type].ElementwiseEquals(ms2));
+        return (ToMS1(ms1_), ToMS2(ms2_));
+    }
+}
