cb6cbd40c0afc6bca9ba76e07de75893cf8e2625
Tarn Yeong Ching <i@ctarn.io>
2025-08-04 17:28:56 +0800

    define `MS1/2` globally

--- src/PepPre.cs
+++ src/PepPre.cs
@@ -1,4 +1,7 @@
-﻿namespace UniMZ.PepPre;
+﻿global using MS1 = (int[] id, System.Memory<UniMZ.Core.Peak<double>>[] peak);
+global using MS2 = (int[] id, System.Memory<UniMZ.Core.Peak<double>>[] peak, int[] pre, double[] mz, double[] r);
+
+namespace UniMZ.PepPre;
 
 public static partial class PepPre
 {
